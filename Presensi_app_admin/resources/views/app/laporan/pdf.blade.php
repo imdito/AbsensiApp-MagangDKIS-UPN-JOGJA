ef979618670d60a8e226aca1722976d8b7d8a7e2
Imdito <123230157@student.upnyk.ac.id>
2026-01-14 15:59:33 +0700

    menambah CRUD pada bidang, merapihkan struktur html menjadi layouting

--- Presensi_app_admin/resources/views/app/laporan/pdf.blade.php
+++ Presensi_app_admin/resources/views/app/laporan/pdf.blade.php
@@ -4,50 +4,53 @@
     <meta charset="UTF-8">
     <title>Laporan Data Karyawan</title>
     <style>
-        /* 1. Reset & Font Dasar */
-        body {
-            font-family: "Times New Roman", Times, serif; /* Font standar surat dinas */
-            margin: 2cm 2cm; /* Margin kertas standar */
-            padding: 0;
-            width: 100%;
-            align-content: flex-start;
+        /* 1. SETUP KERTAS & MARGIN TIPIS */
+        @page {
+            /* Atas-Bawah: 1cm (agar Kop Surat aman saat diprint) */
+            /* Kiri-Kanan: 0.5cm (agar lebar konten MAKSIMAL) */
+            margin: 1cm 0.5cm;
         }
 
-        /* 2. Layout Kop Surat (Gunakan Tabel agar rapi di DomPDF) */
+        body {
+            font-family: "Times New Roman", Times, serif;
+            margin: 0; /* Reset margin body karena sudah diatur di @page */
+            padding: 0;
+        }
+
+        /* 2. HEADER / KOP SURAT */
         .kop-header {
             width: 100%;
             border-collapse: collapse;
             margin-bottom: 5px;
+            /* Pastikan tabel kop juga mengikuti lebar maksimal */
+            table-layout: fixed;
         }
 
-        /* Kolom Logo (Kiri) */
         .kop-logo-cell {
             width: 15%;
             text-align: center;
             vertical-align: middle;
         }
 
-        /* Kolom Teks (Tengah) */
         .kop-text-cell {
-            width: 85%;
+            width: 85%; /* Sisa dari logo (100% - 15%) */
             text-align: center;
             vertical-align: middle;
-            padding-right: 15px; /* Kompensasi visual agar terlihat benar-benar tengah */
+            padding-right: 15px; /* Sedikit padding agar center visual pas */
         }
 
-        /* 3. Styling Teks Kop */
         .pemkot-name {
-            font-size: 16pt;
-            font-weight: bold;
+            font-size: 14pt;
             text-transform: uppercase;
             letter-spacing: 1px;
             margin-bottom: 2px;
         }
         .instansi-name {
-            font-size: 18pt;
+            font-size: 16pt;
             font-weight: bold;
             text-transform: uppercase;
             margin-bottom: 2px;
+
         }
         .alamat-text {
             font-size: 10pt;
@@ -57,29 +60,45 @@
             font-size: 9pt;
         }
 
-        /* 4. Garis Ganda (Khas Surat Dinas) */
         .garis-pemisah {
-            border-top: 3px solid black;    /* Garis tebal di atas */
-            height: 2px;                    /* Spasi antar garis */
-            border-bottom: 1px solid black; /* Garis tipis di bawah */
+            border-top: 3px solid black;
+            height: 2px;
+            border-bottom: 1px solid black;
             margin-bottom: 20px;
         }
 
-        /* 5. Styling Tabel Data (Opsional, untuk isi laporan) */
+        /* 3. TABEL DATA (LEBAR MAKSIMAL) */
         .table-data {
             width: 100%;
             border-collapse: collapse;
             margin-top: 20px;
         }
+
         .table-data th, .table-data td {
             border: 1px solid black;
-            padding: 6px;
-            font-size: 11pt;
+            padding: 4px 6px; /* Padding sedikit diperkecil agar muat banyak */
+            font-size: 10pt;  /* Ukuran font disesuaikan agar rapi */
+            vertical-align: middle;
         }
+
         .table-data th {
             background-color: #f0f0f0;
             text-align: center;
+            font-weight: bold;
+            text-transform: uppercase;
         }
+
+        /* Zebra Striping (Opsional: Agar baris ganjil/genap beda warna dikit biar enak dibaca) */
+        .table-data tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+        .col-no { width: 5%; }
+        .col-nip { width: 20%; }
+        .col-nama { width: 20%; }
+        .col-bidang { width: 33%; }
+        .col-tgl { width: 12%; }
+        .col-ket { width: 10%; }
+
     </style>
 </head>
 <body>
@@ -89,19 +108,19 @@
         <td class="kop-logo-cell">
             <img src="{{ public_path('logo-pemkot.png') }}" width="90" height="auto" alt="Logo">
         </td>
-
         <td class="kop-text-cell">
             <div class="pemkot-name">PEMERINTAH KOTA CIREBON</div>
             <div class="instansi-name">DINAS KOMUNIKASI, INFORMATIKA DAN STATISTIK</div>
             <div class="alamat-text">Jalan Dr. Sudarsono No. 40, Cirebon 45134 Telepon (0231) 8804620, 209324</div>
-            <div class="kontak-text">Email dkis@cirebonkota.go.id Website dkis.cirebonkota.go.id </div>
+            <div class="kontak-text">Email dkis@cirebonkota.go.id Website dkis.cirebonkota.go.id</div>
         </td>
     </tr>
 </table>
 
 <div class="garis-pemisah"></div>
+
 <div style="text-align: center; margin-bottom: 20px;">
-    <h3 style="text-decoration: underline; margin-bottom: 5px;">LAPORAN DATA ASN</h3>
+    <h3 style="text-decoration: underline; margin-bottom: 5px; margin-top: 0;">LAPORAN DATA ASN</h3>
     @if(isset($info['start_date']) && isset($info['end_date']))
         <p style="margin: 0; font-size: 11pt;">Periode: {{ $info['start_date'] }} s/d {{ $info['end_date'] }}</p>
     @endif
@@ -110,23 +129,30 @@
 <table class="table-data">
     <thead>
     <tr>
-        <th width="5%">No</th>
-        <th width="20%">NIP</th>
-        <th width="30%">Nama</th>
-        <th width="20%">Tanggal</th>
-        <th width="25%">Keterangan</th>
+        <th class="col-no text-center">No</th>
+        <th class="col-nip text-center">NIP</th>
+        <th class="col-nama text-left">Nama</th>
+        <th class="col-bidang text-center">Bidang</th>
+        <th class="col-tgl text-center">Tanggal</th>
+        <th class="col-ket text-left">Ket</th>
+
     </tr>
     </thead>
     <tbody>
-    @foreach($data as $index => $item)
+    @forelse($data as $index => $item)
         <tr>
             <td style="text-align: center;">{{ $index + 1 }}</td>
             <td>{{ $item->user->NIP }}</td>
             <td>{{ $item->user->Nama_Pengguna ?? '-' }}</td>
+            <td style="text-align: center">{{$item->user->bidang->nama_bidang}}</td>
             <td style="text-align: center;">{{ $item->tanggal }}</td>
             <td>{{ $item->status }}</td>
         </tr>
-    @endforeach
+    @empty
+        <tr>
+            <td colspan="5" style="text-align: center; font-style: italic;">Tidak ada data pada periode ini.</td>
+        </tr>
+    @endforelse
     </tbody>
 </table>
 

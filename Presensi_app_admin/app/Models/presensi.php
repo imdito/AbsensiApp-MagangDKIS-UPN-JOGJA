5ebe3d1e8c3af188e005c487a6ef9ab4c2482bc8
Imdito <123230157@student.upnyk.ac.id>
2026-01-15 15:20:22 +0700

    Memberikan SoftDeletes pada model dan database

--- Presensi_app_admin/app/Models/presensi.php
+++ Presensi_app_admin/app/Models/presensi.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Enums\Enums;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class presensi extends Model
 {
-    use hasfactory;
+    use hasfactory, softDeletes;
     protected $table = 'presensi';
     public $timestamps = false;
 
@@ -24,8 +25,27 @@ class presensi extends Model
         'jam_pulang',
         'Longitude',
         'Latitude',
-      'created_at'
+      'created_at',
+        'created_id',
+        'updated_id',
+        'deleted_id',
     ];
+
+    protected $dates = ['deleted_at'];
+
+    protected static function booted(): void
+    {
+        static::creating(function ($model) {
+            $model->created_id = auth()->id();
+        });
+        static::updating(function ($model) {
+            $model->updated_id = auth()->id();
+        });
+        static::deleting(function ($model) {
+            $model->deleted_id = auth()->id();
+            $model->save();
+        });
+    }
 
     public function User(){
         return $this->belongsTo(User::class, 'user_id','user_id'); // menambah foreignkey pada table presensi
